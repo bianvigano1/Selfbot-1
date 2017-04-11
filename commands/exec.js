158e8e45393f63819372536e23e96c1db225b204
heysilverblood <heysilverblood@outlook.com>
2017-04-11 16:02:07 +0000

    Let's try that one more time

--- commands/exec.js
+++ commands/exec.js
@@ -1,6 +1,8 @@
+const exec = require('child_process').exec;
+
 module.exports = {
     "name":"exec",
-    "description": "eval a thing!",
+    "description": "exec a thing!",
     "usage": "[SHELL]",
     "run": function(client, msg, args, config) {
 		var code = msg.content.substring(config.prefix.length + this.name.length + 1, 9999)
